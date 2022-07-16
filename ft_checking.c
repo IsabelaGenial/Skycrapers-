70be4283f0d530e2c99440f57b5959c5f56acaca
Isabela Genial <106828234+IsabelaGenial@users.noreply.github.com>
2022-07-16 19:03:14 -0300

    Add files via upload

---
+++ ft_checking.c
@@ -0,0 +1,36 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_checking.c                                      :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ismolina <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/07/16 17:17:25 by ismolina          #+#    #+#             */
+/*   Updated: 2022/07/16 17:57:53 by ismolina         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+#include<stdio.h>
+#include<unistd.h>
+
+void ft_checking(void)
+{
+	int col1up; //l1c2
+	int col2up; //l1c3
+	int col3up; //l1c4
+	int col4up; //l1c5
+ 
+	int col1down; //l6c2
+	int col2down; //l6c3
+	int col3down; //l6c4
+	int col4down; //l6c5
+
+	int row1left; //l2c1
+	int row2left; //l3c1
+	int row3left; //l4c1
+	int row4left; //l5c1
+
+	int row1right; //l2c6
+	int row2right; //l3c6
+	int row3right; //l4c6
+	int row4right; //l5c6
+}
